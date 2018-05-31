66e97226022588b9fc99cac2eb9167dfe82127be
Edwin Rodríguez <noreply@deployer.none>
2018-05-30 22:43:56 -0400

    Automatic deploy

--- lib.js
+++ lib.js
@@ -1,10 +1,12 @@
-var ANIM={};ANIM.modal_levels=0;ANIM.fade_in=function(elem){if(elem.style.opacity==""||elem.style.opacity==0){elem.style.opacity=0;elem.style.transition="opacity 0.5s";requestAnimationFrame(function(){elem.style.display='';requestAnimationFrame(function(){elem.style.opacity=1})});return!0}
-return!1};ANIM.fade_out=function(elem){if(elem.style.opacity==""||elem.style.opacity==1){elem.style.transition="opacity 0.5s";elem.style.opacity=0;setTimeout(function(){if(elem.style.opacity==0)
+var ANIM={modal_levels:0,fadeIn(elem){if(typeof elem==="string")
+elem=document.getElementById(elem);if(elem.style.opacity===null||elem.style.opacity===''||elem.style.opacity==='0'){elem.style.opacity='0';elem.style.transition="opacity 0.5s";requestAnimationFrame(function(){elem.style.display='';requestAnimationFrame(function(){elem.style.opacity='1'})});return!0}
+return!1},fadeOut(elem){if(typeof elem==="string")
+elem=document.getElementById(elem);if(elem.style.opacity===null||elem.style.opacity===''||elem.style.opacity==='1'){elem.style.transition="opacity 0.5s";elem.style.opacity='0';setTimeout(function(){if(elem.style.opacity==='0')
 elem.style.display='none'},500);return!0}
-return!1};ANIM.modal_in=function(elem){if(typeof elem==="string")
-elem=document.getElementById(elem);elem.style.top=0;elem.style.left=0;elem.style.position='fixed';elem.style.width='100%';elem.style.height='100%';if(ANIM.fade_in(elem)){if(ANIM.modal_levels==0)
-document.body.style.overflow='hidden';ANIM.modal_levels++}};ANIM.modal_out=function(elem){if(typeof elem==="string")
-elem=document.getElementById(elem);if(ANIM.fade_out(elem)){if(ANIM.modal_levels==1)
-document.body.style.overflow=null;ANIM.modal_levels--}};ANIM.change_page=function(elem,target){if(typeof elem==="string")
-elem=document.getElementById(elem);var children=elem.children;for(var i=0;i<children.length;i++){var child=children[i];var page_name=child.getAttribute('page_name');if(page_name==target)
-ANIM.fade_in(child);else ANIM.fade_out(child)}}
+return!1},modalIn(elem){if(typeof elem==="string")
+elem=document.getElementById(elem);elem.style.top='0';elem.style.left='0';elem.style.position='fixed';elem.style.width='100%';elem.style.height='100%';if(ANIM.fadeIn(elem)){if(ANIM.modal_levels===0)
+document.body.style.overflow='hidden';ANIM.modal_levels++}},modalOut(elem){if(typeof elem==="string")
+elem=document.getElementById(elem);if(ANIM.fadeOut(elem)){if(ANIM.modal_levels===1){document.body.style.overflow=null}
+ANIM.modal_levels--}},changePage(elem,page_name){if(typeof elem==="string")
+elem=document.getElementById(elem);let children=elem.children;for(let i=0;i<children.length;i++){let child=children[i];let current_page_name=child.getAttribute('data-page-name');if(current_page_name===page_name)
+ANIM.fadeIn(child);else ANIM.fadeOut(child)}}}
